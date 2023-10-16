6a528fd3a985e0f582a054d53411bdd48eb732d6
user <user@10.107.30.180>
2023-10-16 18:53:04 +0800

    First Commit

---
+++ Student/src/com/studyopedia/student.java
@@ -0,0 +1,13 @@
+package com.studyopedia;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nur Qaisara Begum Binti Mohd Yazzed");
+		System.out.println("216405");
+		System.out.println("No.14 Jalan7/3E Seksyen 7 BBB");
+
+	}
+
+}
